51cd45cd318e0615a0c3be955585283c3770f1fa
Luiz Gustavo <54606597+luizg99@users.noreply.github.com>
2022-07-19 23:49:03 -0300

    .

--- Frases/app/src/main/java/com/example/frases/MainActivity.java
+++ Frases/app/src/main/java/com/example/frases/MainActivity.java
@@ -6,28 +6,45 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import java.time.Clock;
 import java.util.Random;
 
+
 public class MainActivity extends AppCompatActivity {
+    int indice = 0;
+    String[] frases = {
+            "Se você faz algo mal feito agora por falta de tempo, o que te faz pensar que terá tempo para refazer depois?",
+            "Mais são as pessoas que desistem, do que as que fracassam. Pra fracassar é preciso tentar até o final",
+            "O verdadeiro conhecimento vem de dentro.",
+            "Se alguém procura a saúde, pergunta-lhe primeiro se está disposto a evitar no futuro as causas da doença; em caso contrário, abstém-te de o ajudar."
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    public void gerarNovaFrase(View view){
-        String[] frases = {
-                "Se você faz algo mal feito agora por falta de tempo, o que te faz pensar que terá tempo para refazer depois?",
-                "O verdadeiro conhecimento vem de dentro.",
-                "Se alguém procura a saúde, pergunta-lhe primeiro se está disposto a evitar no futuro as causas da doença; em caso contrário, abstém-te de o ajudar."
-        };
-
-        int numero = new Random().nextInt(3);
-
 
         TextView texto = findViewById(R.id.txtFrase);
-        texto.setText(frases[numero]);
+        texto.setText(frases[0]);
+    }
+
+    public void fraseAnterior(View view){
+        if (indice > 0) {
+            indice--;
+
+            TextView texto = findViewById(R.id.txtFrase);
+            texto.setText(frases[indice]);
+        }
+    }
+
+    public void proximaFrase(View view){
+
+        if (indice < frases.length - 1) {
+            indice++;
+
+            TextView texto = findViewById(R.id.txtFrase);
+            texto.setText(frases[indice]);
+        }
     }
 
 

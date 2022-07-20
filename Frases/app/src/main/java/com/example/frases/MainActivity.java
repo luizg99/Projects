dc565beba7fd71fbe904c7084d3399a2189bfc4b
Luiz Gustavo <54606597+luizg99@users.noreply.github.com>
2022-07-19 22:41:48 -0300

    .

--- Frases/app/src/main/java/com/example/frases/MainActivity.java
+++ Frases/app/src/main/java/com/example/frases/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.frases;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void gerarNovaFrase(View view){
+        String[] frases = {
+                "Se você faz algo mal feito agora por falta de tempo, o que te faz pensar que terá tempo para refazer depois?",
+                "O verdadeiro conhecimento vem de dentro.",
+                "Se alguém procura a saúde, pergunta-lhe primeiro se está disposto a evitar no futuro as causas da doença; em caso contrário, abstém-te de o ajudar."
+        };
+
+        int numero = new Random().nextInt(3);
+
+
+        TextView texto = findViewById(R.id.txtFrase);
+        texto.setText(frases[numero]);
+    }
+
+
+
 }

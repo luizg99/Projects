79e2f575d7697ea7c695ac317573d31f3b1f5019
LuizG99 <luizfasam@gmail.com>
2022-12-17 10:03:53 -0300

    .

---
+++ Altis/AutomacaoSubirBase/main.py
@@ -0,0 +1,41 @@
+# Importação de bibliotecas.
+import os
+import datetime
+import zipfile
+import time
+import ftplib
+import sys
+
+vUsuario = 'PINHEIRAO'
+
+
+# Abrindo o cmd, utilizando o comando do oracle. Local que o backup será salvo.
+os.system("start /wait cmd /c sqlplus sys as sysdba; ")
+os.system("start /wait cmd /c altis;")
+os.system("start /wait cmd /c alter session set '_oracle_scrip'=TRUE; ")
+os.system("start /wait cmd /c alter system enable restricted session; ")
+os.system("start /wait cmd /c DROP USER vUsuario CASCADE; ")
+os.system("start /wait cmd /c shutdown; ")
+os.system("start /wait cmd /c startup; ")
+#criando usuario
+os.system("start /wait cmd /c create user " +  vUsuario + ";")
+os.system("start /wait cmd /c profile 'DEFAULT' ")
+os.system("start /wait cmd /c identified by 'DADOS'' ")
+os.system("start /wait cmd /c default tablespace 'DADOS'  ")
+os.system("start /wait cmd /c temporary tablespace 'TEMPO' ")
+os.system("start /wait cmd /c quota unlimited on DADOS ")
+os.system("start /wait cmd /c quota unlimited on INDICES ")
+os.system("start /wait cmd /c account unlock; ")
+#Definindo grants do usuário
+os.system("start /wait cmd /c grant CREATE PROCEDURE to " +  vUsuario + ";")
+os.system("start /wait cmd /c grant CREATE TRIGGER to " +  vUsuario + ";")
+os.system("start /wait cmd /c grant CREATE TABLE to " +  vUsuario + ";")
+os.system("start /wait cmd /c grant CREATE VIEW to " +  vUsuario + ";")
+os.system("start /wait cmd /c grant CREATE SEQUENCE to " +  vUsuario + ";")
+os.system("start /wait cmd /c grant CONNECT to " +  vUsuario + ";")
+os.system("start /wait cmd /c grant CREATE JOB to " +  vUsuario + ";")
+os.system("start /wait cmd /c grant CREATE DATABASE LINK to " +  vUsuario + ";")
+os.system("start /wait cmd /c grant CREATE MATERIALIZED VIEW, ALTER ANY MATERIALIZED VIEW, QUERY REWRITE to " +  vUsuario + ";")
+
+
+

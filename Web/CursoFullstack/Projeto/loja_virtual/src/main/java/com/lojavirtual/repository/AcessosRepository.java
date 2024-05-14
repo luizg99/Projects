6e6e538bc6cbf192b067b3772119c71d873f6535
LuizG99 <luizfasam@gmail.com>
2024-05-13 21:59:11 -0300

    Testes

--- Web/CursoFullstack/Projeto/loja_virtual/src/main/java/com/lojavirtual/repository/AcessosRepository.java
+++ Web/CursoFullstack/Projeto/loja_virtual/src/main/java/com/lojavirtual/repository/AcessosRepository.java
@@ -1,6 +1,9 @@
 package com.lojavirtual.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.lojavirtual.model.Acessos;
@@ -11,6 +14,7 @@ import jakarta.transaction.Transactional;
 @Repository
 //Definindo para o repositório gerenciar as transações com o banco de dados.
 @Transactional
-public interface AcessosRepository extends JpaRepository<Acessos, Integer>{
-
+public interface AcessosRepository extends JpaRepository<Acessos, Long>{
+	@Query(value = "select * from acessos a where upper(trim(a.DESCRICAO)) like %?1%", nativeQuery = true)
+	List<Acessos> buscarAcessoDescricao(String descricao);
 }
--- Web/CursoFullstack/Projeto/loja_virtual/src/test/java/com/example/demo/LojaVirtualApplicationTests.java
+++ Web/CursoFullstack/Projeto/loja_virtual/src/test/java/com/example/demo/LojaVirtualApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,23 +10,65 @@ import com.lojavirtual.LojaVirtualApplication;
 import com.lojavirtual.controller.AcessoController;
 import com.lojavirtual.model.Acessos;
 import com.lojavirtual.repository.AcessosRepository;
-import com.lojavirtual.service.AcessosService;
+
+import junit.framework.TestCase;
 
 @SpringBootTest(classes = LojaVirtualApplication.class)
-class LojaVirtualApplicationTests {
+public class LojaVirtualApplicationTests extends TestCase {
+	/*
+	@Autowired
+	private AcessosService acessosService;
 	
 	@Autowired
+	private AcessosRepository acessosRepository; 
+	*/
+	@Autowired
 	private AcessoController acessoController;
+
+	@Autowired
+	private AcessosRepository acessoRepository;
 	
 	@Test
 	public void testeCadastrarAcesso() {
-		Acessos acessos = new Acessos();
+		Acessos acesso = new Acessos();
 		
-		acessos.setDescricao("ROLE_ADMIN");
-		
-		acessoController.salvarAcesso(acessos);
+		acesso.setDescricao("ROLE_ADMIN");
 
+		assertEquals(true, acesso.getId() == null);
+		
+		acesso = acessoController.salvarAcesso(acesso).getBody();
+		
+		assertEquals(true, acesso.getId() > 0);
+		
+		assertEquals("ROLE_ADMIN", acesso.getDescricao());
+		
+		/*Teste de carregamento*/
+		Acessos acesso2 = acessoRepository.findById(acesso.getId()).get();
+		
+		assertEquals(acesso.getId(), acesso2.getId());
+
+		/*teste de delete*/
+		acessoRepository.deleteById(acesso2.getId());
+		
+		acessoRepository.flush();
+		
+//		Acessos acesso3 = acessoRepository.findById(acesso2.getId()).orElse(null);
+		
+//		assertEquals(null, acesso3 == null);
+		
+		acesso = new Acessos();
+		acesso.setDescricao("ROLE_ALUNO");
+		
+		acesso = acessoController.salvarAcesso(acesso).getBody();
+		
+		List<Acessos> acessos = acessoRepository.buscarAcessoDescricao("ALUNO".trim().toUpperCase());
+		
+		assertEquals(true, acessos.size() > 0);
+		
+		acessoRepository.deleteById(acesso.getId());
 	}
+	
+	
 	
 
 }

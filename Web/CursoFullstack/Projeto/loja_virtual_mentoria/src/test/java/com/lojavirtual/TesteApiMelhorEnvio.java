f36f091462714863e4afe3b0ed4aa36435c9d064
Luizito <luizfasam@gmail.com>
2024-09-27 12:52:34 -0300

    Aula 7.4 Comprando Frete e Etiquetas

--- Web/CursoFullstack/Projeto/loja_virtual_mentoria/src/test/java/com/lojavirtual/TesteApiMelhorEnvio.java
+++ Web/CursoFullstack/Projeto/loja_virtual_mentoria/src/test/java/com/lojavirtual/TesteApiMelhorEnvio.java
@@ -1,12 +1,13 @@
 package com.lojavirtual;
 
-import java.awt.PageAttributes.MediaType;
 import java.io.IOException;
 
 import com.lojavirtual.security.ApiTokenIntegracao;
 
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class TesteApiMelhorEnvio {
@@ -14,17 +15,20 @@ public class TesteApiMelhorEnvio {
     public static void main(String[] args) throws IOException {
         OkHttpClient client = new OkHttpClient();
 
+        MediaType mediaType = MediaType.parse("application/json");
+        RequestBody body = RequestBody.create(mediaType, "{\"options\":{\"receipt\":true,\"own_hand\":true,\"reverse\":true,\"non_commercial\":true}}");
         Request request = new Request.Builder()
-                .url(ApiTokenIntegracao.URL_MELHOR_ENVIO_SAND_BOX + "api/v2/me/shipment/companies")
-                .get()
-                .addHeader("accept", "application/json")
-                .addHeader("User-Agent", "luizfasam@gmail.com")
-                .build();
+          .url("https://sandbox.melhorenvio.com.br/api/v2/me/cart")
+          .post(body)
+          .addHeader("Accept", "application/json")
+          .addHeader("Content-Type", "application/json")
+          .addHeader("Authorization", "Bearer token" + ApiTokenIntegracao.TOKEN_MELHOR_ENVIO_SAND_BX)
+          .addHeader("User-Agent", "luizfasam@gmail.com")
+          .build();
 
         Response response = client.newCall(request).execute();
-
-        System.out.println(response.body().string());
-
+    	
+        System.out.print(response.body().string());
     }
-	
+    
 }

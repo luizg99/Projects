8e7c17133fa2a54adb87e5b2aacb4d3decc13520
Luiz Gustavo <54606597+luizg99@users.noreply.github.com>
2024-12-10 11:42:18 -0300

    criando estrutura projeto pizzaria.

---
+++ Web/ReactJsNodeJs/Pizzaria/backend/src/routes.ts
@@ -0,0 +1,12 @@
+import { Router, Request, Response } from 'express';
+
+const router = Router();
+
+router.get('/teste', (req: Request, res: Response) =>{
+    throw new Error('teste')
+    
+    //return res.json({ok: true})
+})
+
+
+export {router}

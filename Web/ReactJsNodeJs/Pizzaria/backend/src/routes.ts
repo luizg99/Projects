1a9880b7b0fd25a74e3977e58a63c5081c865a67
Luiz Gustavo <54606597+luizg99@users.noreply.github.com>
2024-12-11 12:05:46 -0300

    Varias funcionalidades do projeto pizza adicionadas.

--- Web/ReactJsNodeJs/Pizzaria/backend/src/routes.ts
+++ Web/ReactJsNodeJs/Pizzaria/backend/src/routes.ts
@@ -1,12 +1,18 @@
-import { Router, Request, Response } from 'express';
+import { Router } from 'express';
+
+import { CreateUserController } from './controllers/user/CreateUserController'
+import { AuthUserController } from './controllers/user/AuthUserController'
+import { DetailuserController } from './controllers/user/DetailUserController'
+
+import { isAuthenticated } from './middlewares/isAuthenticated'
 
 const router = Router();
 
-router.get('/teste', (req: Request, res: Response) =>{
-    throw new Error('teste')
-    
-    //return res.json({ok: true})
-})
+//-- ROTAS USER --
+router.post('/users', new CreateUserController().handle)
 
+router.post('/session', new AuthUserController().handle)
 
-export {router}
+router.get('/me', isAuthenticated,  new DetailuserController().handle )
+
+export { router }; 

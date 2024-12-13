ec66eb212fe3759267538e40df661b0280c916f0
Luiz Gustavo <54606597+luizg99@users.noreply.github.com>
2024-12-13 14:55:10 -0300

    Mais funcionalidades

--- Web/ReactJsNodeJs/Pizzaria/backend/src/server.ts
+++ Web/ReactJsNodeJs/Pizzaria/backend/src/server.ts
@@ -1,29 +1,34 @@
-import express, {Request, Response, NextFunction, Router} from 'express';
+import express, { Request, Response, NextFunction } from 'express'
 import 'express-async-errors';
-import {router} from './routes';
 import cors from 'cors';
+import path from 'path'
+
+import { router } from './routes'
 
 const app = express();
-
 app.use(express.json());
-
-app.use(cors())
+app.use(cors());
 
 app.use(router);
 
-app.use( (err: Error, req: Request, res: Response, next: NextFunction) => {
+app.use(
+  '/files',
+  express.static(path.resolve(__dirname, '..', 'tmp'))
+)
 
-    if(err instanceof Error){
-        //Se for instancia de erro
-        return res.status(400).json({
-            error: err.message
-        })
-    }
-
-    return res.status(500).json({
-        status: 'error',
-        message: "Internal server error"
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if(err instanceof Error){
+    //Se for uma instancia do tipo error
+    return res.status(400).json({
+      error: err.message
     })
+  }
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal server error.'
+  })
+
 })
 
-app.listen(3333, () => console.log('Server on'))
+app.listen(3333, () => console.log('Servidor online!!!!'))

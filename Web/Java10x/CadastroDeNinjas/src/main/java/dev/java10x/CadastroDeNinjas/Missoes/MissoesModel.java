0349f5a35d3f1f8fa593fffb8c6f4c096b0966fc
Luiz Gustavo <54606597+luizg99@users.noreply.github.com>
2025-05-24 21:26:10 -0300

    Add MissoesRepository

--- Web/Java10x/CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
+++ Web/Java10x/CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -3,6 +3,8 @@ package dev.java10x.CadastroDeNinjas.Missoes;
 import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
 
+import java.util.List;
+
 @Entity
 @Table(name = "Missoes")
 public class MissoesModel {
@@ -15,7 +17,47 @@ public class MissoesModel {
 
     //Uma missão pode ter vários ninjas
     @OneToMany(mappedBy = "missoes")
-    private NinjaModel ninja;
+    private List<NinjaModel> ninjas;
 
+    public MissoesModel() {
+    }
 
+    public MissoesModel(Long id, String nome, String dificuldade, List<NinjaModel> ninjas) {
+        this.id = id;
+        Nome = nome;
+        Dificuldade = dificuldade;
+        this.ninjas = ninjas;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String nome) {
+        Nome = nome;
+    }
+
+    public String getDificuldade() {
+        return Dificuldade;
+    }
+
+    public void setDificuldade(String dificuldade) {
+        Dificuldade = dificuldade;
+    }
+
+    public List<NinjaModel> getNinjas() {
+        return ninjas;
+    }
+
+    public void setNinjas(List<NinjaModel> ninjas) {
+        this.ninjas = ninjas;
+    }
 }

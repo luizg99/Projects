43d1b49e503b95192a107c395ca2b6678ed81fdb
Luiz Gustavo <54606597+luizg99@users.noreply.github.com>
2026-01-04 19:30:59 -0300

    ok.

--- Web/Angular-Udemy/angular-curso-completo-do-iniciante-ao-avancado/conceitos-basicos/src/app/lista-compras/lista-compras.component.ts
+++ Web/Angular-Udemy/angular-curso-completo-do-iniciante-ao-avancado/conceitos-basicos/src/app/lista-compras/lista-compras.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-lista-compras',
   standalone: true,
-  imports: [],
+  imports: [ FormsModule ],
   templateUrl: './lista-compras.component.html',
   styleUrl: './lista-compras.component.scss'
 })
 export class ListaComprasComponent {
-
+  item: string = '';
 }
